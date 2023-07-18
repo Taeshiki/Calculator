7e2aaa98e96bd2cd1eaa50f1e7840ed35fb5a0ac
Taeshiki <durudurutta@gmail.com>
2023-07-18 21:46:33 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 요시킴 on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
