bc45d824bd5bcc004dc65aad57bd792fc3ff045f
Taeshiki <durudurutta@gmail.com>
2023-07-19 21:55:43 +0900

    계산기 만들기 lv1 calculator 클래스에 연산을 메서도로 구현

--- Calculator/CalculatorApp.swift
+++ Calculator/CalculatorApp.swift
@@ -15,3 +15,55 @@ struct CalculatorApp: App {
         }
     }
 }
+
+class Calculator {
+    var firstNumber: Double = 0
+    var secondNumber: Double = 0
+    var result: Double = 0
+    
+    var additionResult: Double {
+        get {
+            return firstNumber + secondNumber
+        }
+        set(operationResult) {
+            result = operationResult
+        }
+    }
+    func addition(firstNumber: Double, secondNumber: Double) -> Double {
+        let result = firstNumber + secondNumber
+        return result
+    }
+    
+    func subtraction(firstNumber: Double, secondNumber: Double) -> Double {
+        let result = firstNumber - secondNumber
+        return result
+    }
+    
+    func multiplication(firstNumber: Double, secondNumber: Double) -> Double {
+        let result = firstNumber * secondNumber
+        return result
+    }
+    
+    func division(firstNumber: Double, secondNumber: Double) -> Double {
+        let result = firstNumber / secondNumber
+        return result
+    }
+    
+}
+
+var calculatorAdditionResult = Calculator().addition(firstNumber: 4, secondNumber: 8)
+
+print(calculatorAdditionResult)
+
+var calculatorSubtractionResult = Calculator().subtraction(firstNumber: 4, secondNumber: 7)
+print(calculatorSubtractionResult)
+
+var calculatormultiplicationResult = Calculator().multiplication(firstNumber: 3, secondNumber: 3)
+print(calculatormultiplicationResult)
+
+var calculatordivisionResult = Calculator().division(firstNumber: 4, secondNumber: 2)
+print(calculatordivisionResult)
+
+
+
+
