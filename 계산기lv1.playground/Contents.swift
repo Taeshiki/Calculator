4630f148c6a1e42e99b53c2e75e3a5fd4fbc06c9
Taeshiki <durudurutta@gmail.com>
2023-07-20 18:52:53 +0900

    Lv3 클래스 나누기

--- 계산기lv1.playground/Contents.swift
+++ 계산기lv1.playground/Contents.swift
@@ -1,45 +1,60 @@
 import UIKit
 
 class Calculator {
+    var plus = Plus()
+    var minus = Minus()
+    var multiplication = Multiplication()
+    var division = Division()
+    var remainder = Remainder()
+    
+    
+}
+
+class Plus {
     func addition(firstNumber: Double, secondNumber: Double) -> Double {
         let result = firstNumber + secondNumber
         return result
     }
-    
+}
+
+class Minus {
     func subtraction(firstNumber: Double, secondNumber: Double) -> Double {
         let result = firstNumber - secondNumber
         return result
     }
-    
+}
+
+class Multiplication {
     func multiplication(firstNumber: Double, secondNumber: Double) -> Double {
         let result = firstNumber * secondNumber
         return result
     }
-    
+}
+
+class Division {
     func division(firstNumber: Double, secondNumber: Double) -> Double {
         let result = firstNumber / secondNumber
         return result
     }
-    
+}
+
+class Remainder {
     func Modulooperation(firstNumber: Double, secondNumber: Double) -> Double {
         let result = firstNumber.truncatingRemainder(dividingBy: secondNumber)
         return result
     }
-    
 }
 
-var calculatorAdditionResult = Calculator().addition(firstNumber: 4.534636, secondNumber: 8.345345)
 
-print(calculatorAdditionResult)
 
-var calculatorSubtractionResult = Calculator().subtraction(firstNumber: 4, secondNumber: 7)
-print(calculatorSubtractionResult)
+var plus = Plus().addition(firstNumber: 6, secondNumber: 7)
+var minus = Minus().subtraction(firstNumber: 8, secondNumber: 5)
+var multiplication = Multiplication().multiplication(firstNumber: 3, secondNumber: 3)
+var division = Division().division(firstNumber: 9, secondNumber: 2)
+var remainder = Remainder()
 
-var calculatormultiplicationResult = Calculator().multiplication(firstNumber: 3, secondNumber: 3)
-print(calculatormultiplicationResult)
+print(plus)
+print(minus)
+print(multiplication)
+print(division)
 
-var calculatordivisionResult = Calculator().division(firstNumber: 4, secondNumber: 7)
-print(calculatordivisionResult)
-
-var ModulooperationResult = Calculator().Modulooperation(firstNumber: 3, secondNumber: 2)
-print(ModulooperationResult)
